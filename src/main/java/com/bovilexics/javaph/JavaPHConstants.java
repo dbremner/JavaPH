c73751a06ecb7135d2b15f9aca69178cd4da1005
David Bremner <dbremner@gmail.com>
2019-05-27 10:40:30 -0700

    remove redundant modifiers

--- src/main/java/com/bovilexics/javaph/JavaPHConstants.java
+++ src/main/java/com/bovilexics/javaph/JavaPHConstants.java
@@ -21,83 +21,83 @@ package com.bovilexics.javaph;
  */
 public interface JavaPHConstants
 {	
-	public static final int APP_DEFAULT_HEIGHT = 700;
-	public static final int APP_DEFAULT_WIDTH  = 700;
-	public static final int APP_MIN_HEIGHT     = 350;
-	public static final int APP_MIN_WIDTH      = 350;
+	int APP_DEFAULT_HEIGHT = 700;
+	int APP_DEFAULT_WIDTH  = 700;
+	int APP_MIN_HEIGHT     = 350;
+	int APP_MIN_WIDTH      = 350;
 
-	public static final String APP_DEFAULT_STATUS = "Waiting for Query";
+	String APP_DEFAULT_STATUS = "Waiting for Query";
 	
-	public static final int QUERY_COMMAND  = 0;
-	public static final int SPLASH_DISPLAY = 3000; // display time in ms
+	int QUERY_COMMAND  = 0;
+	int SPLASH_DISPLAY = 3000; // display time in ms
 
-	public static final int QUERY_RUNTIME_DEF = 20;
-	public static final int QUERY_RUNTIME_MIN = 10;
-	public static final int QUERY_RUNTIME_MAX = 180;
+	int QUERY_RUNTIME_DEF = 20;
+	int QUERY_RUNTIME_MIN = 10;
+	int QUERY_RUNTIME_MAX = 180;
 
-	public static final int RESULT_TEXT_TAB  = 0;
-	public static final int RESULT_TABLE_TAB = 1;
-	public static final int SYSTEM_LOG_TAB   = 2;
+	int RESULT_TEXT_TAB  = 0;
+	int RESULT_TABLE_TAB = 1;
+	int SYSTEM_LOG_TAB   = 2;
 
-	public static final String RESULT_TEXT_LABEL  = "Text Results";
-	public static final String RESULT_TABLE_LABEL = "Table Results";
-	public static final String SYSTEM_LOG_LABEL   = "System Log";
+	String RESULT_TEXT_LABEL  = "Text Results";
+	String RESULT_TABLE_LABEL = "Table Results";
+	String SYSTEM_LOG_LABEL   = "System Log";
 
-	public static final String[] TAB_LABELS =
+	String[] TAB_LABELS =
 	{
 		RESULT_TEXT_LABEL, RESULT_TABLE_LABEL, SYSTEM_LOG_LABEL
 	};
 
-	public static final String[] TAB_FILENAMES =
+	String[] TAB_FILENAMES =
 	{
 		"javaph-result.txt", "javaph-result.csv", "javaph-log.txt"
 	};
 
-	public static final int FIELD_DEFAULT = 0;
-	public static final int FIELD_ALL     = 1;
-	public static final int FIELD_CUSTOM  = 2;
+	int FIELD_DEFAULT = 0;
+	int FIELD_ALL     = 1;
+	int FIELD_CUSTOM  = 2;
 	
-	public static final int LOAD_FIELDS_DEF      = 1;
-	public static final int LOAD_FIELDS_MANUAL   = 0;
-	public static final int LOAD_FIELDS_SELECTED = 1;
-	public static final int LOAD_FIELDS_STARTUP  = 2;
+	int LOAD_FIELDS_DEF      = 1;
+	int LOAD_FIELDS_MANUAL   = 0;
+	int LOAD_FIELDS_SELECTED = 1;
+	int LOAD_FIELDS_STARTUP  = 2;
 	
-	public static final String FIELD_LABEL_PREFIX  = " Return : ";
-	public static final String PORT_LABEL_PREFIX   = " Port : ";
-	public static final String PORT_LABEL_SUFFIX   = " ";
-	public static final String QUERY_LABEL_PREFIX  = " Query : ";
-	public static final String SERVER_LABEL_PREFIX = " Server : ";
-	public static final String SERVER_LABEL_SUFFIX = " ";
+	String FIELD_LABEL_PREFIX  = " Return : ";
+	String PORT_LABEL_PREFIX   = " Port : ";
+	String PORT_LABEL_SUFFIX   = " ";
+	String QUERY_LABEL_PREFIX  = " Query : ";
+	String SERVER_LABEL_PREFIX = " Server : ";
+	String SERVER_LABEL_SUFFIX = " ";
 	
-	public static final String PROP_FILE_DEF = "javaph.default.properties"; 
-	public static final String PROP_FILE     = "javaph.properties";
-	public static final String PROP_HEADER   = "Generated JavaPH Properties File";
+	String PROP_FILE_DEF = "javaph.default.properties";
+	String PROP_FILE     = "javaph.properties";
+	String PROP_HEADER   = "Generated JavaPH Properties File";
 
-	public static final String PROP_APP_HEIGHT      = "javaph.startup.height";
-	public static final String PROP_APP_WIDTH       = "javaph.startup.width";
-	public static final String PROP_APP_X_POSITION  = "javaph.startup.xPosition";
-	public static final String PROP_APP_Y_POSITION  = "javaph.startup.yPosition";
-	public static final String PROP_DEFAULT_LNF     = "javaph.default.lnf";
-	public static final String PROP_DEFAULT_SERVER  = "javaph.default.server";
-	public static final String PROP_DISPLAY_LOG     = "javaph.startup.displayLog";
-	public static final String PROP_DISPLAY_SPLASH  = "javaph.startup.displaySplashWindow";
-	public static final String PROP_DISPLAY_TOOLBAR = "javaph.startup.displayToolbar";
-	public static final String PROP_LOAD_FIELDS     = "javaph.servers.loadFields";
-	public static final String PROP_QUERY_RUNTIME   = "javaph.default.queryRuntime";
-	public static final String PROP_SAVE_POSITION   = "javaph.startup.savePosition";
-	public static final String PROP_ROLL_TOOLBAR    = "javaph.startup.rolloverToolbar";  
+	String PROP_APP_HEIGHT      = "javaph.startup.height";
+	String PROP_APP_WIDTH       = "javaph.startup.width";
+	String PROP_APP_X_POSITION  = "javaph.startup.xPosition";
+	String PROP_APP_Y_POSITION  = "javaph.startup.yPosition";
+	String PROP_DEFAULT_LNF     = "javaph.default.lnf";
+	String PROP_DEFAULT_SERVER  = "javaph.default.server";
+	String PROP_DISPLAY_LOG     = "javaph.startup.displayLog";
+	String PROP_DISPLAY_SPLASH  = "javaph.startup.displaySplashWindow";
+	String PROP_DISPLAY_TOOLBAR = "javaph.startup.displayToolbar";
+	String PROP_LOAD_FIELDS     = "javaph.servers.loadFields";
+	String PROP_QUERY_RUNTIME   = "javaph.default.queryRuntime";
+	String PROP_SAVE_POSITION   = "javaph.startup.savePosition";
+	String PROP_ROLL_TOOLBAR    = "javaph.startup.rolloverToolbar";
 	
-	public static final String COMMA_SEPARATOR  = ",";
-	public static final String CUSTOM_SEPARATOR = "|";
-	public static final String TAB_SEPARATOR    = "\t";
+	String COMMA_SEPARATOR  = ",";
+	String CUSTOM_SEPARATOR = "|";
+	String TAB_SEPARATOR    = "\t";
 
-	public static final String FIELD_QUOTE = "\"";
+	String FIELD_QUOTE = "\"";
 
-	public static final String INFO_AUTHOR    = "Robert Fernandes";
-	public static final String INFO_CONTACT   = "robert@bovilexics.com";
-	public static final String INFO_COPYRIGHT = "Copyright \u00a9 2003  bovilexics.com";
-	public static final String INFO_DATE      = "April 9, 2003";
-	public static final String INFO_HOME      = "http://www.bovilexics.com/software/javaph/";
-	public static final String INFO_NAME      = "JavaPH";
-	public static final String INFO_VERSION   = "v1.0";
+	String INFO_AUTHOR    = "Robert Fernandes";
+	String INFO_CONTACT   = "robert@bovilexics.com";
+	String INFO_COPYRIGHT = "Copyright \u00a9 2003  bovilexics.com";
+	String INFO_DATE      = "April 9, 2003";
+	String INFO_HOME      = "http://www.bovilexics.com/software/javaph/";
+	String INFO_NAME      = "JavaPH";
+	String INFO_VERSION   = "v1.0";
 }
