c625062df2215ba3f1b68aefd03eed91c770a6f3
David Bremner <dbremner@gmail.com>
2019-05-29 18:35:00 -0700

    add a stub test file with JUnit5

---
+++ src/test/java/com/bovilexics/javaph/qi/QiServerFileReaderTest.java
@@ -0,0 +1,18 @@
+package com.bovilexics.javaph.qi;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QiServerFileReaderTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void readServers() {
+    }
+}
