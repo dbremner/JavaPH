3c17eaca87b6dac1a55f00afa624934009113364
David Bremner <dbremner@gmail.com>
2019-06-15 14:17:55 -0700

    move parsing code into factory, change field types to immutable lists

--- src/main/java/com/bovilexics/javaph/qi/QiField.java
+++ src/main/java/com/bovilexics/javaph/qi/QiField.java
@@ -37,37 +37,21 @@ import java.util.*;
  * @author Robert Fernandes robert@bovilexics.com
  *
  */
-public class QiField implements Field
+class QiField implements Field
 {
-	private int length;
+	private final int length;
 
 	private final @NotNull String description;
 
 	private final @NotNull String name;
 
-	private final @NotNull List<String> properties;
+	private final @NotNull ImmutableList<String> properties;
 
-	/**
-	 * Construct a QiField with a given property list and description.
-	 *
-	 * @param someProperties a list of field properties.
-	 * @param description a field description.
-	 *
-	 * @exception QiProtocolException in the event of an error parsing the data.
-	 *
-	 */
-	public QiField(final @NotNull String name, final @NotNull String someProperties, final @NotNull String description) throws QiProtocolException
-	{
-		this.name = name;
-		properties = setProperties(someProperties);
-		this.description = description;
-	}
-
-	public QiField(final @NotNull String name, final int length, final @NotNull Collection<String> properties, final @NotNull String description)
+	QiField(final @NotNull String name, final int length, final @NotNull ImmutableList<String> properties, final @NotNull String description)
 	{
 		this.name = name;
 		this.length = length;
-		this.properties = ImmutableList.copyOf(properties);
+		this.properties = properties;
 		this.description = description;
 	}
 
@@ -89,11 +73,9 @@ public class QiField implements Field
 		return name;
 	}
 
-	@SuppressWarnings("AssignmentOrReturnOfFieldWithMutableType")
 	@Override
 	public @NotNull List<String> getProperties()
 	{
-		// This is an immutable list.
 		return properties;
 	}
 
@@ -107,50 +89,6 @@ public class QiField implements Field
 	public int hashCode()
 	{
 		return name.hashCode();
-	}
-
-	/**
-	 * Parse a QI field property.
-	 *
-	 * @param someProperties The properties string which Qi returns in response to a
-	 *         "fields" command. The protocol stuff should be stripped
-	 *         leaving just the field description
-	 *  	   (e.g. "max 64 Indexed Lookup Public Default Any")
-	 *
-	 * @exception QiProtocolException in the event of an error parsing the data.
-	 *
-	 * @return properties collection
-	 */
-	private @NotNull List<String> setProperties(final @NotNull String someProperties) throws QiProtocolException
-	{
-		final @NotNull StringTokenizer tokenizer = new StringTokenizer(someProperties);
-		final @NotNull String token = (String) tokenizer.nextElement();
-
-		if (token.startsWith("max"))
-		{
-			final @NotNull String lengthString = (String) tokenizer.nextElement();
-			try
-			{
-				length = Integer.valueOf(lengthString);
-			}
-			catch (final @NotNull NumberFormatException e)
-			{
-				throw new QiProtocolException("Invalid value for max length property: " + someProperties);
-			}
-		}
-		else
-		{
-			length = -1;
-		}
-
-		final @NotNull ImmutableList.Builder<String> builder = new ImmutableList.Builder<>();
-		// Okay, here come the properties...
-		while (tokenizer.hasMoreElements())
-		{
-			builder.add((String) tokenizer.nextElement());
-		}
-		final @NotNull List<String> list = builder.build();
-		return list;
 	}
 
 	@Override
