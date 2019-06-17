98fdb8f72410246a4021a9a8dede818cd49a81e5
David Bremner <dbremner@gmail.com>
2019-06-16 17:14:19 -0700

    add QiReplyCode

---
+++ src/main/java/com/bovilexics/javaph/qi/QiReplyCodeType.java
@@ -0,0 +1,11 @@
+package com.bovilexics.javaph.qi;
+
+public enum QiReplyCodeType
+{
+    Status,
+    Information,
+    AdditionalInformationOrAction,
+    TemporaryError,
+    PermanentError,
+    PhQueryCode;
+}
