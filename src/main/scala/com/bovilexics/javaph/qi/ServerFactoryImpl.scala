2af68ba5f9d26106d2439f09b084cf91784a3bad
David Bremner <dbremner@gmail.com>
2019-06-19 07:06:51 -0700

    move code to companion object and add throws annotations

--- src/main/scala/com/bovilexics/javaph/qi/ServerFactoryImpl.scala
+++ src/main/scala/com/bovilexics/javaph/qi/ServerFactoryImpl.scala
@@ -11,21 +11,6 @@ object ServerFactoryImpl
   @Contract(pure = true)
   private def isValidPort(port: Int): Boolean =
     port >= 0 && port <= 65535
-}
-
-final class ServerFactoryImpl(val fieldFactory: FieldFactory) extends ServerFactory
-{
-  @throws[IllegalArgumentException]
-  override def create(name: String, server: String, port: Int): Server =
-  {
-    if (!isValidPort(port))
-      throw new IllegalArgumentException(PORT_ERROR)
-    new QiServer(fieldFactory, name, server, port)
-  }
-
-  @throws[IllegalArgumentException]
-  override def create(name: String, server: String, port: String) : Server =
-    new QiServer(fieldFactory, name, server, convertToPort(port))
 
   @throws[IllegalArgumentException]
   private def convertToPort(aPort: String) =
@@ -43,4 +28,19 @@ final class ServerFactoryImpl(val fieldFactory: FieldFactory) extends ServerFact
       case _: NumberFormatException =>
         throw new IllegalArgumentException(PORT_ERROR)
     }
+}
+
+final class ServerFactoryImpl(val fieldFactory: FieldFactory) extends ServerFactory
+{
+  @throws[IllegalArgumentException]
+  override def create(name: String, server: String, port: Int): Server =
+  {
+    if (!isValidPort(port))
+      throw new IllegalArgumentException(PORT_ERROR)
+    new QiServer(fieldFactory, name, server, port)
+  }
+
+  @throws[IllegalArgumentException]
+  override def create(name: String, server: String, port: String) : Server =
+    new QiServer(fieldFactory, name, server, ServerFactoryImpl.convertToPort(port))
 }
