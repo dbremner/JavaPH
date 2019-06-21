d5154beb996af9a71419f8c3302b3ad9905772f2
David Bremner <dbremner@gmail.com>
2019-06-21 06:48:45 -0700

    inline method

--- src/main/java/com/bovilexics/javaph/qi/QiLine.java
+++ src/main/java/com/bovilexics/javaph/qi/QiLine.java
@@ -67,7 +67,85 @@ public final class QiLine implements Line
 	public QiLine(final @NotNull String line) throws QiProtocolException
 	{
 		verbatim = line;
-		parse();
+
+		/*
+		TODO : this code doesn't deal with all of the scenarios listed here
+
+		ph myname return what
+		-507:what:unknown field.
+		500:Did not understand ph.
+
+		:
+		598:::Command not recognized.
+
+		what:
+		598:what::Command not recognized.
+
+		what
+		598:what:Command not recognized.
+
+		ph myname return email
+		102:There was 1 match to your request.
+		-200:1:    email: myemail
+		200:Ok.
+
+		quit
+		200:Bye!
+		*/
+
+		// Get the result code.
+		// Index of first colon --> -200:
+		final int colon1Index = verbatim.indexOf(':');
+		if (colon1Index == -1) {
+            throw new QiProtocolException(verbatim);
+        }
+
+		try
+		{
+			code = Integer.parseInt(verbatim.substring(0, colon1Index));
+		}
+		catch (final @NotNull NumberFormatException e)
+		{
+			throw new QiProtocolException(verbatim);
+		}
+
+		// Get the index count, if there is one.
+		// Index of second colon --> -200:1:
+		final int colon2Index = verbatim.indexOf(':', colon1Index + 1);
+		if (colon2Index == -1)
+		{
+			// This isn't a field:value response but rather a one line description.
+			// Just record it and return.
+			response = verbatim.substring(colon1Index + 1);
+			return;
+		}
+		else
+		{
+			try
+			{
+				index = Integer.parseInt(verbatim.substring(colon1Index + 1, colon2Index));
+			}
+			catch (final @NotNull NumberFormatException e)
+			{
+				// This isn't a field:value response but rather a one line description.
+				// Just record it and return.
+				response = verbatim.substring(colon1Index + 1);
+				return;
+			}
+		}
+
+		// This should be a field:value response.
+		// Get field, value and return.
+		// Index of third colon --> -200:1:    email:
+		final int colon3Index = verbatim.indexOf(':', colon2Index + 1);
+		if (colon3Index == -1) {
+            throw new QiProtocolException(verbatim);
+        }
+
+		field = verbatim.substring(colon2Index + 1, colon3Index);
+		value = verbatim.substring(colon3Index + 1);
+		trimmedField = field.trim();
+		trimmedValue = value.trim();
 	}
 
 	@Override
@@ -112,87 +190,6 @@ public final class QiLine implements Line
 		return value;
 	}
 
-	/*
-		TODO : parse() doesn't deal with all of the scenarios listed here
-
-		ph myname return what
-		-507:what:unknown field.
-		500:Did not understand ph.
-		
-		:
-		598:::Command not recognized.
-		
-		what:
-		598:what::Command not recognized.
-		
-		what
-		598:what:Command not recognized.
-		
-		ph myname return email
-		102:There was 1 match to your request.
-		-200:1:    email: myemail
-		200:Ok.
-		
-		quit
-		200:Bye!
-	*/
-	private void parse() throws QiProtocolException
-	{
-		// Get the result code.
-		// Index of first colon --> -200:
-		final int colon1Index = verbatim.indexOf(':');
-		if (colon1Index == -1) {
-            throw new QiProtocolException(verbatim);
-        }
-
-		try
-		{
-			code = Integer.parseInt(verbatim.substring(0, colon1Index));
-		}
-		catch (final @NotNull NumberFormatException e)
-		{
-			throw new QiProtocolException(verbatim);
-		}
-
-		// Get the index count, if there is one.
-		// Index of second colon --> -200:1:
-		final int colon2Index = verbatim.indexOf(':', colon1Index + 1);
-		if (colon2Index == -1)
-		{
-			// This isn't a field:value response but rather a one line description.
-			// Just record it and return.
-			response = verbatim.substring(colon1Index + 1);
-			return;
-		}
-		else
-		{
-			try
-			{
-				index = Integer.parseInt(verbatim.substring(colon1Index + 1, colon2Index));
-			}
-			catch (final @NotNull NumberFormatException e)
-			{
-				// This isn't a field:value response but rather a one line description.
-				// Just record it and return.
-				response = verbatim.substring(colon1Index + 1);
-				return;
-			}
-		}
-		
-		// This should be a field:value response.
-		// Get field, value and return.
-		// Index of third colon --> -200:1:    email:
-		final int colon3Index = verbatim.indexOf(':', colon2Index + 1);
-		if (colon3Index == -1) {
-            throw new QiProtocolException(verbatim);
-        }
-			
-		field = verbatim.substring(colon2Index + 1, colon3Index);
-		value = verbatim.substring(colon3Index + 1);
-		trimmedField = field.trim();
-		trimmedValue = value.trim();
-	}
-	
 	@Override
     public @NotNull String toString()
 	{
