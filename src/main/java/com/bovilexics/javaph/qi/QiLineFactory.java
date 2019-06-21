48d69af3144f6512ca8ca97970891da94868fe13
David Bremner <dbremner@gmail.com>
2019-06-20 22:42:58 -0700

    add @Override

--- src/main/java/com/bovilexics/javaph/qi/QiLineFactory.java
+++ src/main/java/com/bovilexics/javaph/qi/QiLineFactory.java
@@ -4,6 +4,7 @@ import org.jetbrains.annotations.NotNull;
 
 public final class QiLineFactory implements LineFactory
 {
+    @Override
     public @NotNull Line create(final @NotNull String buffer) throws QiProtocolException
     {
         final @NotNull Line line = new QiLine(buffer);
