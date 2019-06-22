6889c514589f00f079bab1b9af1100e74abc52d1
David Bremner <dbremner@gmail.com>
2019-06-22 13:02:16 -0700

    add throws

--- src/main/java/com/bovilexics/javaph/qi/ServerFactory.java
+++ src/main/java/com/bovilexics/javaph/qi/ServerFactory.java
@@ -4,7 +4,7 @@ import org.jetbrains.annotations.NotNull;
 
 public interface ServerFactory
 {
-    @NotNull Server create(final @NotNull String name, final @NotNull String server, final int port);
+    @NotNull Server create(final @NotNull String name, final @NotNull String server, final int port) throws IllegalArgumentException;
 
-    @NotNull Server create(final @NotNull String name, final @NotNull String server, final @NotNull String port);
+    @NotNull Server create(final @NotNull String name, final @NotNull String server, final @NotNull String port)  throws IllegalArgumentException;
 }
