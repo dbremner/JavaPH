d5ccf16a33c2eadd1dfa58cac489acae4fbed271
David Bremner <dbremner@gmail.com>
2019-06-21 19:19:07 -0700

    add comment

--- src/main/java/com/bovilexics/javaph/qi/LineFactory.java
+++ src/main/java/com/bovilexics/javaph/qi/LineFactory.java
@@ -4,5 +4,11 @@ import org.jetbrains.annotations.NotNull;
 
 public interface LineFactory
 {
+    /** Given a line as read from Qi, construct a Line.
+     *
+     * @param buffer as read from Qi, unaltered.
+     *
+     * @exception QiProtocolException if the line can't be parsed.
+     */
     @NotNull Line create(final @NotNull String buffer) throws QiProtocolException;
 }
