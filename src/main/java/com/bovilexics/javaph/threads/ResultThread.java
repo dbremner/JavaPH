0436a64eef5bc0371bda139d25387fe6ce585240
David Bremner <dbremner@gmail.com>
2019-06-23 11:12:41 -0700

    simplify expression

--- src/main/java/com/bovilexics/javaph/threads/ResultThread.java
+++ src/main/java/com/bovilexics/javaph/threads/ResultThread.java
@@ -204,7 +204,7 @@ public final class ResultThread extends Thread
 
 	public synchronized @NotNull List<List<Line>> getRecords()
 	{
-		if (!(state == ResultThreadState.RS_OK))
+		if (state != ResultThreadState.RS_OK)
 		{
 			return ImmutableList.of();
 		}
