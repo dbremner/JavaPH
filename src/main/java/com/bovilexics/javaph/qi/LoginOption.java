30775e99ada1c6ff85ff4bae97067b4a84b04b2a
David Bremner <dbremner@gmail.com>
2019-06-28 12:44:29 -0700

    replace constants with enum

--- src/main/java/com/bovilexics/javaph/qi/LoginOption.java
+++ src/main/java/com/bovilexics/javaph/qi/LoginOption.java
@@ -2,17 +2,21 @@ package com.bovilexics.javaph.qi;
 
 public enum LoginOption
 {
-    ;
-    // LoginQi options
-    public static final int LQ_AUTO		= 0x001; // attempt auto login
-    public static final int LQ_INTER		= 0x002; // allowed to be interactive if needed.
-    public static final int LQ_FWTK		= 0x004; // use FWTK auth server, if available
-    public static final int LQ_KRB4		= 0x008; // use v4 Kerberos login, if available
-    public static final int LQ_KRB5		= 0x010; // use v5 Kerberos login, if available
-    public static final int LQ_GSS		= 0x020; // use GSS-API login, if available
-    public static final int LQ_EMAIL		= 0x040; // use email login, if available
-    public static final int LQ_PASSWORD	= 0x080; // password encrypted response to challenge
-    public static final int LQ_CLEAR		= 0x100; // use clear-text passwords
+    Auto(0x001),        // attempt auto login
+    Inter(0x002),       // allowed to be interactive if needed.
+    Fwtk(0x004),        // use FWTK auth server, if available
+    Krb4(0x008),        // use v4 Kerberos login, if available
+    Krb5(0x010),        // use v5 Kerberos login, if available
+    Gss(0x020),         // use GSS-API login, if available
+    Email(0x040),       // use email login, if available
+    Password(0x080),    // password encrypted response to challenge
+    Clear(0x100),       // use clear-text passwords
+    All(0x004 | 0x008 | 0x010 | 0x020 | 0x040 | 0x080 | 0x100);
 
-    public static final int LQ_ALL		= (LQ_FWTK | LQ_KRB4 | LQ_KRB5 | LQ_GSS | LQ_EMAIL | LQ_PASSWORD | LQ_CLEAR);
+    private final int value;
+
+    LoginOption(final int value)
+    {
+        this.value = value;
+    }
 }
