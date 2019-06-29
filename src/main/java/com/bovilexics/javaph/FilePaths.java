a767d324827e895f2696251422f21700451282e8
David Bremner <dbremner@gmail.com>
2019-06-29 14:12:09 -0700

    add @NonNull

--- src/main/java/com/bovilexics/javaph/FilePaths.java
+++ src/main/java/com/bovilexics/javaph/FilePaths.java
@@ -6,7 +6,7 @@ public class FilePaths
 {
     public static final @NotNull String HELP_FILE_PATH = "help/index.html";
     public static final @NotNull String IMG_PH_ICON_SMALLER_GIF_FILEPATH = "img/ph-icon-smaller.gif";
-    public static final String IMG_CUT_GIF_FILEPATH = "img/cut.gif";
-    public static final String IMG_COPY_GIF_FILEPATH = "img/copy.gif";
-    public static final String IMG_PASTE_GIF_FILEPATH = "img/paste.gif";
+    public static final @NotNull String IMG_CUT_GIF_FILEPATH = "img/cut.gif";
+    public static final @NotNull String IMG_COPY_GIF_FILEPATH = "img/copy.gif";
+    public static final @NotNull String IMG_PASTE_GIF_FILEPATH = "img/paste.gif";
 }
