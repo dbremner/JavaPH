4d38d8480ad4c5c3a2cc1f876fc1c19fbcad802f
David Bremner <dbremner@gmail.com>
2019-07-04 14:43:30 -0700

    include the parent exception

--- src/main/scala/com/bovilexics/javaph/qi/FieldFactoryImpl.scala
+++ src/main/scala/com/bovilexics/javaph/qi/FieldFactoryImpl.scala
@@ -33,8 +33,8 @@ object FieldFactoryImpl
         }
         catch
         {
-          case _: NumberFormatException =>
-          throw new IllegalArgumentException("Invalid value for max length property: " + someProperties)
+          case e: NumberFormatException =>
+          throw new java.lang.IllegalArgumentException("Invalid value for max length property: " + someProperties, e)
         }
       }
       else
