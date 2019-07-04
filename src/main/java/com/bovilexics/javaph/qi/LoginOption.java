a22c51246ce453d679186d046dc1c7fd15ac5cc7
David Bremner <dbremner@gmail.com>
2019-07-04 08:33:24 -0700

    rename enum values to match Java conventions

--- src/main/java/com/bovilexics/javaph/qi/LoginOption.java
+++ src/main/java/com/bovilexics/javaph/qi/LoginOption.java
@@ -2,16 +2,16 @@ package com.bovilexics.javaph.qi;
 
 public enum LoginOption
 {
-    Auto(0x001),        // attempt auto login
-    Inter(0x002),       // allowed to be interactive if needed.
-    Fwtk(0x004),        // use FWTK auth server, if available
-    Krb4(0x008),        // use v4 Kerberos login, if available
-    Krb5(0x010),        // use v5 Kerberos login, if available
-    Gss(0x020),         // use GSS-API login, if available
-    Email(0x040),       // use email login, if available
-    Password(0x080),    // password encrypted response to challenge
-    Clear(0x100),       // use clear-text passwords
-    All(0x004 | 0x008 | 0x010 | 0x020 | 0x040 | 0x080 | 0x100);
+    AUTO(0x001),        // attempt auto login
+    INTER(0x002),       // allowed to be interactive if needed.
+    FWTK(0x004),        // use FWTK auth server, if available
+    KRB4(0x008),        // use v4 Kerberos login, if available
+    KRB5(0x010),        // use v5 Kerberos login, if available
+    GSS(0x020),         // use GSS-API login, if available
+    EMAIL(0x040),       // use email login, if available
+    PASSWORD(0x080),    // password encrypted response to challenge
+    CLEAR(0x100),       // use clear-text passwords
+    ALL(0x004 | 0x008 | 0x010 | 0x020 | 0x040 | 0x080 | 0x100);
 
     private final int value;
 
